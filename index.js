ac879361b5f7fb8e4ba47eb14d6301fea83af255
Soohyun Lim <d2axasap@gmail.com>
2021-03-15 23:12:24 -0400

    Add node devtool to npm script

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require('express');
+const app = express(); // create app object
 const mongoose = require('mongoose');
-const port = process.env.PORT || 5000;
-
-const app = express();
+const bodyParser = require('body-parser');
 const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users.js');
+const tweets = require('./routes/api/tweets.js');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
 
 mongoose
-  .connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World'));
+// app will be listening for incoming get request
+app.get('/', (req, res) => {
+  // debugger;
+  res.send('Hello World!');
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
