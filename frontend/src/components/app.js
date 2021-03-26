c55f515bd71b46145f905315a4c66b6a591350bf
Soohyun Lim <d2axasap@gmail.com>
2021-03-26 02:52:37 -0400

    Add protected routes for three new components

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,9 +3,13 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util.js';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container.js';
 
+import TweetsContainer from './tweets/tweets_container';
 import MainPage from './main/main_page.js';
 import LoginFormContainer from './session/login_form_container.js';
 import SignupFormContainer from './session/signup_form_container.js';
+import ProfileContainer from './profile/profile_container.js';
+import TweetComposeContainer from './tweets/tweet_compose_container.js';
+
 
 const App = () => (
   <div>
@@ -16,6 +20,10 @@ const App = () => (
       {/* render components with forms that allows users to sign up or login */}
       <AuthRoute exact path='/login' component={LoginFormContainer} />
       <AuthRoute exact path='/signup' component={SignupFormContainer} />
+
+      <ProtectedRoute exact path='/tweets' component={TweetsContainer} />
+      <ProtectedRoute exact path='/profile' component={ProfileContainer} />
+      <ProtectedRoute exact path='/new_tweet' component={TweetComposeContainer} />
     </Switch>
   </div>
 );
